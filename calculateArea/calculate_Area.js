bd190970d99dff916137396eb6fe96c3dd03aead
het-suthar-19 <hetsuthar.edu@gmail.com>
2025-04-23 12:28:57 -0400

    message

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -6,6 +6,20 @@ function calculateArea() {
     width = parseFloat(document.getElementById('width').value);
    
    let area = length * width;
+   document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
 
-document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+let item1
+let item2
+let item3
+
+function groceryTracker() {
+    item1 = parseFloat(document.getElementById('item1').value);
+    item2 = parseFloat(document.getElementById('item2').value);
+    item3 = parseFloat(document.getElementById('item3').value);
+
+    let totalAmount = item1 + item2 + item3;
+    document.getElementById('total').innerHTML = `total amount is $${totalAmount}`; 
+}
+
+
